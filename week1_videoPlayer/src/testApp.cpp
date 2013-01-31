2f557f55f10230f3ebbcd3bbf200b5a539d9279e
mariapaulasaba <msd403@nyu.edu>
2013-01-31 14:16:59 -0500

    starting the repo

---
+++ week1_videoPlayer/src/testApp.cpp
@@ -0,0 +1,63 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    my_video.loadMovie("natureCode1.mov");
+    my_video.play();
+    
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    my_video.draw(10,10);
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
