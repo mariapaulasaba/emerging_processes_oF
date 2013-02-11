a07f6065cd52378e829f36c8cedcfeac0400ce5e
mariapaulasaba <msd403@nyu.edu>
2013-02-10 21:28:05 -0500

    deleting

--- week1_assignment/src/square.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  square.cpp
-//  week1_assignment
-//
-//  Created by Maria Paula Saba dos Reis on 2/4/13.
-//
-//
-
-#include "square.h"
--- week2_slitScan_exercise/src/ofxSlitScan.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ofxSlitScan.cpp
-//  week2_slitScan_exercise
-//
-//  Created by Maria Paula Saba dos Reis on 2/9/13.
-//
-//
-
-#include "ofxSlitScan.h"
